e98a1159fbe1275b51ceb059d168c9920ed20471
falsitas <leelee0325@snu.ac.kr>
2026-05-09 00:39:34 +0900

    feat: login

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # 나중에 옮길 것
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def test_view(request):
     return JsonResponse({"message": "Hello, World!"})
+
+@csrf_exempt
+@api_view(['POST'])
+def login(request):
+
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    user = authenticate(
+        username=username,
+        password=password
+    )
+
+    if user is None:
+        return Response({
+            "error": "Invalid username or password"
+        }, status=401)
+
+    refresh = RefreshToken.for_user(user)
+
+    return Response({
+        "access": str(refresh.access_token),
+        "refresh": str(refresh),
+        "username": user.username,
+    })
