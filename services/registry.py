c14efe04dbf76933c7d4913f614ad1c93032f051
falsitas <leelee0325@snu.ac.kr>
2026-05-12 23:57:23 +0900

    fix: user.group access

--- services/registry.py
+++ services/registry.py
@@ -1,9 +1,14 @@
-from agent import LangChainTest
+from services.agent import LangChainTest
 
 agents = dict()
 
 def get_agent(user):
     '''Get or create an agent for the given user. by its Group.'''
-    if user.group not in agents:
-        agents[user.group] = LangChainTest(group=user.group)
-    return agents[user.group]
+    if user.groups.filter(name='ALPHA').exists():
+        group = "ALPHA"
+    else:
+        group = "BETA"
+
+    if group not in agents:
+        agents[group] = LangChainTest(group=group)
+    return agents[group]
